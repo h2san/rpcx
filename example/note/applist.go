6d6f82e6348f2ffe97360e88a39f6e2c92b6abc1
hillguo(郭青山) <hillguo@tencent.com>
2019-05-08 22:12:51 +0800

    ci

---
+++ example/note/applist.go
@@ -0,0 +1,7 @@
+package note
+
+import "context"
+
+func (svr *Note) AppList (ctx context.Context, req *AppListReq, resp *AppListResp) error {
+	return nil
+}
---
+++ example/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"github.com/h2san/rpcx/protocol/rpcx"
+	"github.com/h2san/rpcx/server"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8972", "server address")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+	p :=&rpcx.RPCXProtocol{}
+	
+	s.Protocol = p
+	s.Serve("tcp", "127.0.0.1:8972")
+}
